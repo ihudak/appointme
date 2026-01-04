35042620f9c5235a3ef91f2f018c96c4e76507fa
Ivan Gudak <ihudak@gmail.com>
2026-01-04 22:27:36 +0100

    Add application tests

---
+++ businesses/src/test/java/eu/dec21/appointme/businesses/BusinessesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dec21.appointme.businesses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BusinessesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ users/src/test/java/eu/dec21/appointme/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dec21.appointme.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
