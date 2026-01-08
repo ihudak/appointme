bc7e52fc0d63ed6c2bbbaa52500467d7bbce5528
Ivan Gudak <ihudak@gmail.com>
2026-01-08 23:14:57 +0100

    Rearange and optimize dependencies

--- users/src/test/java/eu/dec21/appointme/users/UsersApplicationTests.java
+++ users/src/test/java/eu/dec21/appointme/users/UsersApplicationTests.java
@@ -2,8 +2,10 @@ package eu.dec21.appointme.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootTest
+@EnableAsync
 public class UsersApplicationTests {
 
     @Test
